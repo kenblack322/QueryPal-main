7a3a542d2bff455c4065242673d3e19af37e1a23
kenblack322 <107812253+kenblack322@users.noreply.github.com>
2025-09-12 16:05:27 +0200

    Update industry.js

--- public/pages/industry/industry.js
+++ public/pages/industry/industry.js
@@ -4,6 +4,11 @@
   function initBasicSwipers() {
     if (typeof Swiper === 'undefined') return;
 
+    if (window.innerWidth > 479) {
+      console.log('Swiper initialization skipped because window width is greater than 479px.');
+      return;
+    }
+
     new Swiper('#basic-swiper', {
       slidesPerView: 1,
       slidesPerGroup: 1,
