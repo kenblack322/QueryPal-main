c0a4c63974eb6d04d781398d733ca1bd38aa29fd
kenblack322 <107812253+kenblack322@users.noreply.github.com>
2025-10-18 22:36:52 +0200

    Update products.js

--- public/pages/products/products.js
+++ public/pages/products/products.js
@@ -1,5 +1,5 @@
 function feedbackSlider() {
-  const swiperMain = new Swiper('swiper-review', {
+  const swiperMain = new Swiper('#feedbackMainSwiper', {
     spaceBetween: 0,
     slidesPerView: 1,
     slidesPerGroup: 1,
